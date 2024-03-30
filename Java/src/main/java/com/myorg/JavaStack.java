190173d7cdbc3db7ffb5c0edecc1ef31b5068da1
naritomo08 <naritomo08@gmail.com>
2024-03-30 16:39:49 +0900

    upload

--- Java/src/main/java/com/myorg/JavaStack.java
+++ Java/src/main/java/com/myorg/JavaStack.java
@@ -4,6 +4,11 @@ import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+
 public class JavaStack extends Stack {
     public JavaStack(final Construct parent, final String id) {
         this(parent, id, null);
@@ -12,5 +17,16 @@ public class JavaStack extends Stack {
     public JavaStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
+        // Lambda
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+            .runtime(Runtime.NODEJS_20_X)
+            .code(Code.fromAsset("lambda"))
+            .handler("hello.handler")
+            .build();
+        
+        // API Gateway
+        LambdaRestApi.Builder.create(this, "Endpoint")
+            .handler(hello)
+            .build();
     }
 }
--- README.md
+++ README.md
@@ -138,6 +138,12 @@ cdk init sample-app --language java
 以下のファイルに各種作成したいソースコードを記載する
 src/main/java/com/myorg/JavaStack.java
 
+必要に応じ、mvnコマンドを使用するためのSW導入
+brew install maven
+
+mvnパッケージ導入(必要に応じ)
+mvn package
+
 CDK アプリケーションからテンプレートを合成
 (AWS CloudFormation テンプレート確認)
 cdk synth
@@ -147,6 +153,12 @@ cdk bootstrap
 
 ソースデプロイ
 cdk deploy
+
+ソースデプロイ(ホットスワップ、本番で使用しないこと。)
+cdk deploy --hotswap
+
+ソースを監視しながらの自動デプロイ実施
+cdk watch
 
 現在の環境とのソース差分確認
 cdk diff
@@ -180,6 +192,12 @@ cdk bootstrap
 
 ソースデプロイ
 cdk deploy
+
+ソースデプロイ(ホットスワップ、本番で使用しないこと。)
+cdk deploy --hotswap
+
+ソースを監視しながらの自動デプロイ実施
+cdk watch
 
 現在の環境とのソース差分確認
 cdk diff
